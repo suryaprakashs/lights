85fc56101aa0f45efe6f070d859cb9660d5395a0
Suryaprakash <suryaprakash.star@gmail.com>
2018-06-06 11:59:19 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# lights
+# Lights
 Capture your moment with C# and Task Scheduler
+
+A simple console exe with Task Scheduler scheduling to capture what you are doing throughout the day.
---
+++ SimpleMan.Lights/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleMan.Lights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "\\";
+            if(args.Count() > 0)
+            {
+                path = args[0];                
+            }
+
+            if (!path.EndsWith("\\"))
+                path += "\\";
+
+            if (path == "\\")
+                path = "";
+
+            CaptureCurrentScreen(path);
+        }
+
+        private static void CaptureCurrentScreen(string path)
+        {
+            try
+            {
+                Rectangle bounds = Screen.GetBounds(Point.Empty);
+                using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+                {
+                    using (Graphics g = Graphics.FromImage(bitmap))
+                    {
+                        g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                    }
+                    bitmap.Save(path + GetFileName(), ImageFormat.Jpeg);
+                }
+            }
+            catch (Exception)
+            {
+                Environment.Exit(1);
+            }
+        }
+
+        private static string GetFileName()
+        {
+            string currentTimeString = DateTime.Now.ToString("yyyyMMddhhmmss");
+            return currentTimeString + ".jpg";
+        }
+    }
+}
